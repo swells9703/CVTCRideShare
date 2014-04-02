05b6c0da2e88ded6b373953b464ae69088f75f2f
Ben Anderson <banderson129@student.cvtc.edu>
2014-04-02 14:35:31 +0000

    Adds Rails Skeleton
    Adds a Rails v4.0 Skeleton for us to build the application on.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CVTCRideShare::Application.config.secret_key_base = 'b8c593879cc64c05d800d8ba7dde25b49fda3216806cc655e7400ed05d4d77cdc9c1a76706c63a45ca5fd1647b923fcde5e22dcaa1417b146355276974c55dea'
