3764a7b22aeeff07a57f5d10dbc61f4ab2ae0e48
Quang Le <quang.le320@yahoo.com>
2014-04-09 17:35:46 +0000

    Adds new user method

--- app/controllers/ride_controller.rb
+++ app/controllers/ride_controller.rb
@@ -2,10 +2,18 @@ class RideController < ApplicationController
   def index
     
   end
+
   def search
 
   end
-  def new
 
+  #TODO notify on view if save fail 
+  def new
+    @ride = Ride.new(params[:ride]) 
+    if @ride.save 
+      redirect_to @ride
+    else
+      render "new"
   end
+
 end
