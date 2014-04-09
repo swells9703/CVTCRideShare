e607c52f6bdd811b8cc315ae67024bed14cda872
Ben <banderson129@student.cvtc.edu>
2014-04-09 16:24:55 -0500

    Adds controller methods and tests

--- app/controllers/ride_controller.rb
+++ app/controllers/ride_controller.rb
@@ -12,12 +12,19 @@ class RideController < ApplicationController
   end
 
   def create
-    @ride = Ride.new(params[:ride]) 
+    @ride = Ride.new(ride_params)
     if @ride.save 
       redirect_to @ride
     else
       render "new"
     end
+  end
+
+  private
+
+  #rails 4 way to do things
+  def ride_params
+    params.permit(:Driver_ID, :Start_Address, :End_Address, :Time)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 CVTCRideShare::Application.routes.draw do
+  resources :ride
   root :to => 'index#index'
+
 end
