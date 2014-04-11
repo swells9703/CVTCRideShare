55200623a69d46eba36b1be4ebe411ae13656a11
Devon Jones <djones9898@hotmail.com>
2014-04-11 22:50:42 +0000

    adds can_search_by_start_address test to ride_test.rb

--- test/models/ride_test.rb
+++ test/models/ride_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class RideTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can search by :Start_Address" do
+    tmpRide = Ride.new
+    tmpRide.Start_Address = "1234 1st Ave, Eau Claire, WI, 54703"
+    
+    searchedRides = Ride.search(:Start_Address => "Eau Claire")
+   end
+  
 end
