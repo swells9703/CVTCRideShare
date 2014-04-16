dbbf3e7acfef521a4843a577b292322aca5838cd
Quang P. Le <quang.le320@yahoo.com>
2014-04-15 22:45:46 -0500

    "Adds navigations bar for the index and the ride and also for the routes"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 CVTCRideShare::Application.routes.draw do
-
+  
+  root :to => "index#index"
+  get "ride/index"
+  get "ride/new"
+  get "ride/search"
+  get "ride/results"
+  
   resources :ride do
     collection do
       get 'search'
