d0b4d959a716e08c56a477ee2550e46facb2282d
Devon Jones <djones9898@hotmail.com>
2014-04-16 13:38:17 +0000

    fixes searching for rides by date

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,11 +3,12 @@ class Ride < ActiveRecord::Base
 
   
   def self.search(args)
+    logger.debug Date.parse(args[:Time].map{|k,v| v }.join("-"))
     tmpRides = Ride.all
     args[:Time].delete_if {|k,v| not v.present? } if args[:Time].present?
     tmpRides = tmpRides.where("Start_Address like ?", "%" + args[:Start_Address] + "%") if args[:Start_Address].present?
     tmpRides = tmpRides.where("End_Address like ?", "%" + args[:End_Address] + "%") if args[:End_Address].present?
-    tmpRides = tmpRides.where("Date(Time) = Date(?)", args[:Time].map{|k,v| v }.join("-")) if args[:Time].present?
+    tmpRides = tmpRides.where("Date(Time) = Date(?)", Date.parse(args[:Time].map{|k,v| v }.join("-"))) if args[:Time].present?
     return tmpRides
     
   end
