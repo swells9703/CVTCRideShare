15e003005f40c6e3f497fdb27166f99f42952fdb
Quang P. Le <quang.le320@yahoo.com>
2014-04-28 19:24:56 -0500

    "Fixes request table in migrate db"

--- db/migrate/20140427202840_ride_request.rb
+++ db/migrate/20140427202840_ride_request.rb
@@ -2,11 +2,9 @@ class RideRequest < ActiveRecord::Migration
   def change
   
     create_table :request do |t|
-      t.integer :Unique_Identifier
       t.integer :Ride_ID
-      t.integer :Requester_ID
+      t.integer :User_ID
       t.boolean :confirmed, default: false
-      t.string :preferences
       t.string :token
     end
     
